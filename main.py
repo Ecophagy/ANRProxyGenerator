24ea95b340a2800c6cd5d126db970ab342834ed6
Ecophagy <olly.spoon@gmail.com>
2017-06-16 21:20:16 +0100

    Now takes deck id from command line
    -Moved everything inside a main function
    -Added functions to check for input flags
    -Added a usage statement
    -ID of deck to proxy is now passed with -d

--- main.py
+++ main.py
@@ -1,44 +1,73 @@
 import requests
 from PIL import Image
-from resizeimage import resizeimage
 from io import BytesIO
 import math
+import sys
+import getopt
 
-base_url = "https://netrunnerdb.com/api/2.0/public/"
-resize_height = 346
-resize_width = 243
+base_url = "https://netrunnerdb.com/api/2.0/public/deck/"
+resize_height = 325
+resize_width = 225
 
-deck_request = requests.get(base_url + "deck/752593")
-if deck_request.status_code == 200:
-    deck_data = deck_request.json()
+usage = 'main.py -d <deck id>'
 
-    proxy_list = []
-    for card_id, number in deck_data['data'][0]['cards'].items():
-        card_picture = requests.get("http://netrunnerdb.com/card_image/" + card_id + ".png")
-        resized_card_picture = Image.open(BytesIO(card_picture.content)).convert("RGBA")
-        resized_card_picture = resized_card_picture.resize((resize_width, resize_height), Image.LANCZOS)
-        for cards in range (0, number):
-            proxy_list.append(resized_card_picture)
 
-    proxy_index = 0
+def main(argv):
+    deck_id = -1
+    try:
+        opts, args = getopt.getopt(argv, 'd:', ["deckid="]) #Get the deck id from the command line
 
-    for sheet_count in range (0, math.ceil(len(proxy_list)/9)): # how many pages do we need?
-        sheet = Image.new('RGBA', (resize_width *3, resize_height * 3)) # a sheet is 3 rows of 3 cards
-        y_offset = 0
-        rows = [Image.new('RGBA', (resize_width * 3, resize_height))] * 3
-        for row in rows:
-            x_offset = 0
+        for opt, arg in opts:
+            print(opt)
+            if opt in ("-d", "--deckid"):
+                deck_id = arg
+            else:
+                print ("Unsupported argument found!")
 
-            for j in range (proxy_index, proxy_index+3):
-                if j >= len(proxy_list):
-                    break
-                row.paste(proxy_list[j], (x_offset,0))
-                x_offset += resize_width
+        deck_request = requests.get(base_url + str(deck_id))
+        if deck_request.status_code == 200:
+            deck_data = deck_request.json()
 
-            sheet.paste(row, (0, y_offset))
-            y_offset += resize_height
-            proxy_index += 3
-            if proxy_index >= len(proxy_list):
-                break
+            proxy_list = []
+            for card_id, number in deck_data['data'][0]['cards'].items():
+                card_picture = requests.get("http://netrunnerdb.com/card_image/" + card_id + ".png")
+                resized_card_picture = Image.open(BytesIO(card_picture.content)).convert("RGBA")
+                resized_card_picture = resized_card_picture.resize((resize_width, resize_height), Image.LANCZOS)
+                for cards in range (0, number):
+                    proxy_list.append(resized_card_picture)
 
-        sheet.save('img_' + str(sheet_count)+ '.png', 'PNG', quality=90)
+            proxy_index = 0
+
+            for sheet_count in range (0, math.ceil(len(proxy_list)/9)): # how many pages do we need?
+                sheet = Image.new('RGBA', (resize_width *3, resize_height * 3)) # a sheet is 3 rows of 3 cards
+                y_offset = 0
+                rows = [Image.new('RGBA', (resize_width * 3, resize_height))] * 3
+                for row in rows:
+                    x_offset = 0
+
+                    for j in range (proxy_index, proxy_index+3):
+                        if j >= len(proxy_list):
+                            break
+                        row.paste(proxy_list[j], (x_offset,0))
+                        x_offset += resize_width
+
+                    sheet.paste(row, (0, y_offset))
+                    y_offset += resize_height
+                    proxy_index += 3
+                    if proxy_index >= len(proxy_list):
+                        break
+
+                sheet.save('img_' + str(sheet_count)+ '.png', 'PNG', quality=90)
+        else:
+            print("Error: Could not retrieve decklist")
+
+    except getopt.GetoptError as e:
+        print(str(e))
+        print(usage)
+        sys.exit(2)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print(usage)
+    else:
+        main(sys.argv[1:])
